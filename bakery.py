fc0ac8c742b2215aeb01437218a951e47f4c3444
IleiaRezaei <63117966+IleiaRezaei@users.noreply.github.com>
2020-11-01 20:43:08 -0800

    first commit

---
+++ bakery.py
@@ -0,0 +1,25 @@
+bread = {
+  "sangak" : 20,
+  "barbari" : 15,
+  "lavash" : 13,
+  "taftoon" : 25
+}
+
+print("we have sagak, barbari, lavash, and taftoon")
+sum = 0
+
+answer = "yes"
+while answer != "no":
+  buy = input("what do you want?")
+
+  price = bread[buy]
+  print("That will be", price, "each")
+  count = input("How many do you want? ")
+  finalprice = int(count) * price
+  print("That will be", finalprice)
+  sum = sum + finalprice
+  
+
+  answer = input("Do you want anything else?")
+
+print("your total is", sum)
